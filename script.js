5c620f1a8bfa68045ef6d1928c78fa405bd477be
Miriam Seu <miriams@missionreadyhq.com>
2024-09-04 16:29:02 +1200

    mission 2 submission up to date

---
+++ script.js
@@ -0,0 +1,18 @@
+// "use strict"
+console.log("Connected")
+
+// === GALLERY SECTION === //
+
+const images = [
+    'images/1-orchid.jpg',
+    'images/2-tulip.webp',
+    'images/3-dahlia.jpg',
+    'images/4-carnation.jpg',
+    'images/5-sunflower.jpg',
+    'images/6-iris.jpg',
+    'images/7-begonia.webp',
+    'images/8-peonies.webp',
+    'images/9-zinnia.jpg',
+    'images/10-alstroemeria.jpg'
+];
+
